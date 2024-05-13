2716ea50170375c241e6bb817700dd0d8bafa2ad
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-13 23:15:56 +0200

    Add mongoose connection

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const mongoDB = process.env.MONGODB_STRING;
 const expressLayouts = require('express-ejs-layouts');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const clubhouseRouter = require('./routes/clubhouse');
 
 const app = express();
 
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/clubhouse', clubhouseRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
