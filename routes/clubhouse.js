4156071305737bf3b3818fe8e0939e5f55b3657e
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-14 13:08:27 +0200

    Add sign-up/log-in routes

--- routes/clubhouse.js
+++ routes/clubhouse.js
@@ -6,4 +6,9 @@ const post_controller = require('../controllers/postController');
 
 router.get('/', post_controller.index);
 
+router.get('/user/sign-up', user_controller.sign_up_get);
+router.post('/user/sign-up', user_controller.sign_up_post);
+router.get('/user/log-in', user_controller.log_in_get);
+router.post('/user/log-in', user_controller.log_in_post);
+
 module.exports = router;
