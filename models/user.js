41f0d5d4785935144eed63f36a5764e938db5fe3
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-14 13:07:31 +0200

    Update user model

--- models/user.js
+++ models/user.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  first_name: { type: String, required: true, minLength: 1, maxLength: 20 },
-  last_name: { type: String, required: true, minLength: 1, maxLength: 20 },
-  username: { type: String, required: true, minLength: 3, maxLength: 20 },
-  password: { type: String, required: true, minLength: 8, maxLength: 20 },
-  membership_status: {
+  first_name: { type: String, required: true, minLength: 1, maxLength: 100 },
+  last_name: { type: String, required: true, minLength: 1, maxLength: 100 },
+  username: { type: String, required: true, minLength: 5, maxLength: 100 },
+  password: { type: String, required: true, minLength: 5, maxLength: 100 },
+  status: {
     type: String,
     required: true,
     minLength: 1,
