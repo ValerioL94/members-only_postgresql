2cfec06bfe0e0255ee39338af28ded0ace311025
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-15 17:41:01 +0200

    Add new message controllers

--- controllers/postController.js
+++ controllers/postController.js
@@ -7,3 +7,11 @@ exports.index = asyncHandler(async (req, res, next) => {
   const allPosts = await Post.find().exec();
   res.render('index', { title: 'Homepage', user: req.user });
 });
+
+exports.create_message_get = asyncHandler(async (req, res, next) => {
+  res.render('create_message', { title: 'New Message', user: req.user });
+});
+exports.create_message_post = [
+  body(),
+  asyncHandler(async (req, res, next) => {}),
+];
