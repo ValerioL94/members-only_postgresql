4b86bf50b319a11ed35d91644e53140288fb0c1c
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-15 11:13:48 +0200

    Update model

--- models/user.js
+++ models/user.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   first_name: { type: String, required: true, minLength: 1, maxLength: 100 },
   last_name: { type: String, required: true, minLength: 1, maxLength: 100 },
-  username: { type: String, required: true, minLength: 5, maxLength: 100 },
-  password: { type: String, required: true, minLength: 5, maxLength: 100 },
+  email: { type: String, required: true, minLength: 1, maxLength: 100 },
+  password: { type: String, required: true, minLength: 8, maxLength: 100 },
   status: {
     type: String,
     required: true,
-    minLength: 1,
+    enum: ['anon', 'member', 'admin'],
     default: 'anon',
   },
 });
