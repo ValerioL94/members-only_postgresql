08e71768fd84c38bcd77450c1b90243577b88048
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-08-18 20:38:24 +0200

    add database connection

---
+++ db/pool.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+module.exports = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  user: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+});
