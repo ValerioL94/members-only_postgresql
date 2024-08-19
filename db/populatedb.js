6d3326faa331fe9c680f4f4d8566646c30b19429
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-08-19 16:40:00 +0200

    add populatedb script

---
+++ db/populatedb.js
@@ -0,0 +1,40 @@
+const { Client } = require('pg');
+const { CONNECTION_STRING } = process.env;
+
+const SQL1 = `
+CREATE TYPE IF NOT EXISTS status AS ENUM ('Anonymous', 'Exclusive Member', 'Admin');
+
+CREATE TABLE IF NOT EXISTS users (
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+first_name VARCHAR(50) NOT NULL,
+last_name VARCHAR(50) NOT NULL,
+email VARCHAR(50) NOT NULL UNIQUE,
+password VARCHAR(100) NOT NULL,
+status status DEFAULT ('Anonymous'),
+);
+`;
+
+const SQL2 = `
+CREATE TABLE IF NOT EXISTS posts (
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+title VARCHAR (50) NOT NULL,
+comment VARCHAR(200) NOT NULL,
+user_id INTEGER REFERENCES users,
+timestamp TIMESTAMP WITH TIME ZONE DEFAULT now()
+);
+`;
+
+async function main() {
+  console.log('seeding...');
+  const client = new Client({
+    connectionString: CONNECTION_STRING,
+  });
+  await client.connect();
+  await client.query(SQL1);
+  await client.query(SQL2);
+
+  await client.end();
+  console.log('done');
+}
+
+main();
