e9356011469b306b75a8e0ab2ad06d35c254e270
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-08-19 16:14:49 +0200

    update routers

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,9 +7,10 @@ router.post('/sign-up', user_controller.sign_up_post);
 router.get('/log-in', user_controller.log_in_get);
 router.post('/log-in', user_controller.log_in_post);
 router.get('/log-out', user_controller.log_out_get);
+/*
 router.get('/join-the-club', user_controller.join_get);
 router.post('/join-the-club', user_controller.join_post);
 router.get('/admin-page', user_controller.admin_get);
 router.post('/admin-page', user_controller.admin_post);
-
+*/
 module.exports = router;
